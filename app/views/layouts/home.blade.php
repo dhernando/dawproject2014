33e5a2bb505f708384b753df954a43c18f954555
temaserio <miguelhervias92@gmail.com>
2014-04-30 19:08:36 +0200

    new home

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -1,53 +1,40 @@
 <!DOCTYPE html>
 <html lang='en'>
     <head>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="assets/ico/favicon.png">
+
         <title>@yield('title')</title>
-        <!-- Librerias -->
-        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-        <script src="{{ URL::asset('js/bootstrap.js') }}"></script>
+
         <!-- CSS -->
         <link rel='stylesheet' href="{{ URL::asset('css/admin.css')}}">
-        <link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'>
         <link rel="stylesheet" href="{{ URL::asset('css/header.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('css/body.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('css/footer.css') }}">
+        <link href="{{ URL::asset('css/bootstrap.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('css/soon.css') }}" rel="stylesheet">
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-    </head>
-    <body>
-        <nav class="navbar navbar-default" role="navigation">
-          <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">
-                <span class="glyphicon glyphicon-music imageweb"></span>
-                <span class="nameweb">Machacasaurio</span>
-              </a>
-            </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav navbar-right">
-                <li class="active"><img class="avatar" src="img/avatar.jpg"><a class="username" href="#">Arturito Mas</a></li>
-              </ul>
-            </div><!-- /.navbar-collapse -->
-          </div><!-- /.container-fluid -->
-        </nav>
-        <div class='container-fluid'>
-            <div class='row'>
-                @yield('content')
-            </div>
-        </div>
-        <nav class="navbar-fixed-bottom footerdiv" role="navigation">
-          <div class="container">
-            Footer text, etc
-          </div>
-        </nav>
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="assets/js/html5shiv.js"></script>
+          <script src="assets/js/respond.min.js"></script>
+        <![endif]-->
+        
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+    </head>
+    <body class="nomobile">
+        @yield('content')
+        <!-- Librerias -->
+        <script src="{{ URL::asset('js/jquery.min.js') }}"></script>
+        <script type="{{ URL::asset('js/modernizr.custom.js') }}"></script>
+        <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+        <script src="{{ URL::asset('js/soon/plugins.js') }}"></script>
+        <script src="{{ URL::asset('js/soon/jquery.themepunch.revolution.min.js') }}"></script>
+        <script src="{{ URL::asset('js/soon/custom.js') }}"></script>
     </body>
 </html>
